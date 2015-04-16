b8e9f97142cc017eb9d5c13df2c346ca10a3eaee
Nolawee Mengist <nome9478@colorado.edu>
2015-04-15 19:32:00 -0600

    Initial Commit

---
+++ Assignment9/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment9
+//
+//  Created by Nolawee Mengist on 4/15/15.
+//  Copyright (c) 2015 nope. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
